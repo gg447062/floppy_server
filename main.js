6e569db0733f4e0e7dbb9800e3d2c00438328d23
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-09-28 11:04:26 -0600

    replaces socket.io with ws

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const app = require('./server');
 const dotenv = require('dotenv').config();
 const socket = require('socket.io');
+const { WebSocketServer } = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,32 +10,47 @@ const server = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
-const io = socket(server, {
-  cors: {
-    origin: '*',
-  },
-});
+const wss = new WebSocketServer({ server });
 
-io.on('connection', (socket) => {
+wss.on('connection', (ws, req) => {
   const ip =
-    socket.handshake.headers['x-forwarded-for'] ||
-    socket.conn.remoteAddress.split(':')[3];
-  console.log('new connection:', socket.id, ip);
+    req.headers['x-forwarded-for'] || req.socket.remoteAddress.split(':')[3];
 
-  socket.on('disconnect', (reason) => {
-    console.log('socket disconnected:', reason);
+  console.log('new connection:', ip);
+  ws.on('error', console.error);
+  ws.on('message', (data) => {
+    const { room, message } = JSON.parse(data);
+    console.log(`recieved ${message} for room ${room}`);
   });
-  socket.emit('message', 'welcome');
-
-  socket.on('SetRecordState', (msg, callback) => {
-    console.log('Setting Recording State:', msg);
-    // Check if 'msg' is true
-    if (msg) {
-      socket.emit('message', 'record');
-      callback('Server is recording!');
-    } else {
-      socket.emit('message', 'stop');
-      callback('Server is not recording!');
-    }
-  });
+  ws.send('welcome');
 });
+
+// const io = socket(server, {
+//   cors: {
+//     origin: '*',
+//   },
+// });
+
+// io.on('connection', (socket) => {
+//   const ip =
+//     socket.handshake.headers['x-forwarded-for'] ||
+//     socket.conn.remoteAddress.split(':')[3];
+//   console.log('new connection:', socket.id, ip);
+
+//   socket.on('disconnect', (reason) => {
+//     console.log('socket disconnected:', reason);
+//   });
+//   socket.emit('message', 'welcome');
+
+//   socket.on('SetRecordState', (msg, callback) => {
+//     console.log('Setting Recording State:', msg);
+
+//     if (msg) {
+//       socket.emit('message', 'record');
+//       callback('Server is recording!');
+//     } else {
+//       socket.emit('message', 'stop');
+//       callback('Server is not recording!');
+//     }
+//   });
+// });
